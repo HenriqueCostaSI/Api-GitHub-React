c5548a66f5b9756f3dbee32e24b9a34f16170cb9
henriqueCostaSI <henrique.mcosta@hotmail.com>
2025-02-11 23:03:13 -0300

    Adiciona estado para controle do input de busca na MainPage

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdSearch } from "react-icons/md";
 import { Container, Logo, Title, Form, Input, Button } from "./styles";
 import githubLogo from "../../assets/images/github-logo.svg";
 
+
 function MainPage() {
+  const [login, setLogin] = useState("");
   return (
     <Container>
       <Logo src={githubLogo} alt="API Github" />
       <Title>API GitHub</Title>
       <Form>
-        <Input type="text" placeholder="Digite o nome do usuário" />
+        <Input type="text" placeholder="Digite o nome do usuário" 
+         value={login} onChange={(e) => setLogin(e.target.value)} />
         <Button> 
           <MdSearch size={42} color="#fff" />
         </Button>
